a1c42b95acdea91da8770079998c2a84356ae781
SikandarJODD <sikandar6174@gmail.com>
2025-03-03 19:14:25 +0530

    style: Landing Page

--- src/lib/landing/comps/Features.svelte
+++ src/lib/landing/comps/Features.svelte
@@ -10,42 +10,47 @@
   import Section from "./Section.svelte";
   import Card from "$lib/components/ui/card/card.svelte";
   import CardContent from "$lib/components/ui/card/card-content.svelte";
+  import Edit from "$lib/images/edit.svelte";
+  import Code from "$lib/images/code.svelte";
+  import Schema from "$lib/images/schema.svelte";
 
   let problems = [
     {
       title: "Schema Generation",
       description:
         "We use Zod and Valibot, powerful schema-based libraries, to validate data dynamically-building schemas tailored to each user's input!",
-      icon: ShieldCheck,
+      icon: Schema,
     },
     {
       title: "Client & Server Code",
       description:
         "Our platform provides client and server-side code generation, allowing you to focus on building your application.",
-      icon: FileCode2,
+      icon: Code,
     },
     {
       title: "Customization",
       description:
         "Customize form inputs with attributes like min, max, required, labels, descriptions, and more for a seamless user experience! 🚀",
-      icon: FilePenLine,
+      icon: Edit,
     },
   ];
 </script>
 
 <Section title="Features" subtitle="Formsnap + Superforms + Schema">
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-8 mt-4">
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-4 sm:gap-8 mt-0 sm:mt-4">
     {#each problems as problem, index}
       {@const Icon = problem.icon}
       <Card class="bg-background border-none shadow-none">
-        <CardContent class="p-6 space-y-4">
+        <CardContent class="p-4 md:p-6 space-y-2 sm:space-y-4">
           <div
-            class="w-12 h-12 bg-primary/10 rounded-full flex items-center justify-center"
+            class="w-12 h-12 text-primary bg-primary/10 rounded-full flex items-center justify-center"
           >
-            <Icon class="w-6 h-6 text-primary" />
+            <Icon  />
           </div>
-          <h3 class="text-xl font-semibold">{problem.title}</h3>
-          <p class="text-muted-foreground">{problem.description}</p>
+          <div class="space-y-1.5">
+            <h3 class="text-xl font-semibold">{problem.title}</h3>
+            <p class="text-muted-foreground">{problem.description}</p>
+          </div>
         </CardContent>
       </Card>
     {/each}
--- src/lib/landing/comps/Section.svelte
+++ src/lib/landing/comps/Section.svelte
@@ -21,7 +21,7 @@
 
 <section>
   <div class={_class}>
-    <div class="relative container mx-auto px-4 py-16 max-w-7xl">
+    <div class="relative container mx-auto px-4 md:pb-16 pt-16 max-w-7xl">
       <div class="text-center space-y-4 pb-6 mx-auto">
         <h2
           class="text-sm text-primary font-mono font-medium tracking-wider uppercase"
@@ -29,7 +29,7 @@
           {title}
         </h2>
         <h3
-          class="mx-auto mt-4 max-w-xs text-3xl font-semibold sm:max-w-none sm:text-4xl md:text-5xl"
+          class="mx-auto mt-4 max-w-xs text-2xl font-semibold sm:max-w-none sm:text-4xl md:text-5xl"
         >
           {subtitle}
         </h3>
