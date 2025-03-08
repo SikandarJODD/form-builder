5b89828ed3045ce171cea19472f64f59c059aa36
SikandarJODD <sikandar6174@gmail.com>
2025-03-08 14:14:21 +0530

    feat: Radio Group Setup

--- src/routes/try/+page.svelte
+++ src/routes/try/+page.svelte
@@ -1,19 +1,12 @@
 <script lang="ts">
-  import { superForm } from "sveltekit-superforms";
-  // add your own path
-  import type { PageData } from "./$types";
-  import Label from "$lib/components/ui/label/label.svelte";
-  import Button from "$lib/components/ui/button/button.svelte";
-  import Input from "$lib/components/ui/input/input.svelte";
+  import * as RadioGroup from "$lib/components/ui/radio-group/index";
+  import { Label } from "$lib/components/ui/label/index";
 
-  // Location Input
-  let selectedCountry_loc = $state(null);
-  let selectedState_loc = $state(null);
-
-  // Form Validation & Schema
-  import { zod } from "sveltekit-superforms/adapters";
+  import { zodClient } from "sveltekit-superforms/adapters";
   import { schema } from "./schema";
-  import LocationSelector from "$lib/components/templates/comps/location-input/LocationSelector.svelte";
+  import type { PageData } from "./$types";
+  import { superForm } from "sveltekit-superforms";
+  import Button from "$lib/components/ui/button/button.svelte";
 
   let {
     data,
@@ -21,64 +14,40 @@
     data: PageData;
   } = $props();
 
-  let { form, message, errors, enhance } = superForm(data.form, {
-    validators: zod(schema),
-    dataType: "json",
-    onUpdated(event) {
-      if (event.form.valid) {
-        selectedCountry_loc = null;
-        selectedState_loc = null;
-      }
-    },
+  let { form, enhance, errors } = superForm(data.form, {
+    validators: zodClient(schema),
   });
 </script>
 
-<div class="flex min-h-[80vh] flex-col items-center justify-center">
-  {#if $message}
-    <p class="text-emerald-400 mb-2">{$message}</p>
-  {/if}
-  <!-- Location Input Component : https://svelte-form-builder.vercel.app/docs/components/location-input -->
-  <form method="post" use:enhance class="w-full md:w-96 space-y-2 p-4 lg:p-0">
+<div class="flex justify-center items-center min-h-screen">
+  <form method="POST" use:enhance class="space-y-2 min-w-96">
+    <div class="space-y-3">
+      <Label class="text-sm font-medium">Gender</Label>
+      <RadioGroup.Root bind:value={$form.radio} name="radio">
+        <div class="flex items-center space-x-2">
+          <RadioGroup.Item value="male" id="r1" />
+          <Label for="r1">Male</Label>
+        </div>
+        <div class="flex items-center space-x-2">
+          <RadioGroup.Item value="female" id="r2" />
+          <Label for="r2">Female</Label>
+        </div>
+        <div class="flex items-center space-x-2">
+          <RadioGroup.Item value="other" id="r3" />
+          <Label for="r3">Other</Label>
+        </div>
+      </RadioGroup.Root>
+      <div>
+        <p class="text-xs text-muted-foreground">Select your Gender</p>
+        <div>
+          {#if $errors.radio}
+            <p class="text-red-500 text-xs">{$errors.radio}</p>
+          {/if}
+        </div>
+      </div>
+    </div>
     <div>
-      <Label for="text" class={$errors.text && "text-destructive"}
-        >Username</Label
-      >
-      <Input
-        type="text"
-        id="text"
-        name="text"
-        placeholder="Enter your username"
-        bind:value={$form.text}
-      />
-      {#if $errors.text}
-        <p class="text-sm text-destructive">{$errors.text}</p>
-      {/if}
-      <p class="text-xs text-muted-foreground">
-        This is your public display name
-      </p>
+      <Button size="sm" type="submit">Submit</Button>
     </div>
-
-    <div class="min-w-full">
-      <Label for="loc">Location</Label>
-      <LocationSelector
-        bind:selectedCountry={selectedCountry_loc}
-        bind:selectedState={selectedState_loc}
-        onCountryChange={(country) => {
-          $form.loc.country = country?.name ?? "";
-        }}
-        onStateChange={(state) => {
-          $form.loc.state = state?.name ?? "";
-        }}
-      />
-      <p class="text-sm text-muted-foreground">
-        Select your country and state from the dropdown.
-      </p>
-      <input type="hidden" name="country" bind:value={$form.loc.country} />
-      <input type="hidden" name="state" bind:value={$form.loc.state} />
-      {#if $errors.loc?.country}
-        <p class="text-sm text-destructive">{$errors.loc.country}</p>
-      {/if}
-    </div>
-    <Button type="submit" size="sm">Submit</Button>
   </form>
 </div>
--- src/routes/try/schema.ts
+++ src/routes/try/schema.ts
@@ -1,8 +1,6 @@
 import { z } from 'zod';
 export let schema = z.object({
-  text: z.string().nonempty(),
-  loc: z.object({
-    country: z.string().nonempty('Please enter a country name.'),
-    state: z.string().optional().default(''),
-  }),
+  radio: z.enum(['male', 'female', 'other'], {
+    required_error: 'Please select a radio option',
+  })
 })
